9d0ebcf62b130661a44ce7edf78b40cd94eb7d07
Dominique Danco <ddanco@gmail.com>
2016-12-13 14:33:56 -0500

    edited setup, deleted old logos

--- setup.py
+++ setup.py
@@ -5,22 +5,19 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='speechtools',
-      version='0.5.0',
+setup(name='PyLing',
+      version='1.0',
       description='',
       long_description='',
       classifiers=[
-        'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Text Processing :: Linguistic',
       ],
-      keywords='speech corpus phonetics',
-      url='https://github.com/MontrealCorpusTools/speechcorpustools',
-      author='Montreal Corpus Tools',
-      author_email='michael.e.mcauliffe@gmail.com',
+      keywords='vocabulary flashcards english french',
+      url='https://github.com/ddanco/PyLing',
+      author='Dominique Danco',
+      author_email='ddanco@gmail.com',
       packages=['pyling'],
       install_requires=[
           'PyQt5',
